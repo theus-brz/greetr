e104f00ca4bec765d3b5e17d6fb4cfd3def60f80
Matheus Barbosa <matheusbrbs@gmail.com>
2021-01-28 20:31:23 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var g = G$('John', 'Doe');
+g.greet();
+
+$('#login').click(function() {
+  var loginGrtr = G$('John', 'Doe');
+
+  $('#logindiv').hide();
+
+  loginGrtr
+    .setLang($('#lang').val())
+    .HTMLGreeting('#greeting', true)
+    .log();
+})
